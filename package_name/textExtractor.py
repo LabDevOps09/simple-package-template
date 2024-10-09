20bdaa2fc138fbffeaba1d0f0862d02273b01c78
MaryDias <166772912+LabDevOps09@users.noreply.github.com>
2024-10-09 09:47:34 -0300

    Update and rename codigoDoPacote.py to textExtractor.py

--- package_name/textExtractor.py
+++ package_name/textExtractor.py
@@ -2,22 +2,16 @@ from PIL import Image
 import pytesseract
 import cv2
 
-def preprocess_image(image_path, output_path=None):
+def preprocess_image(image_path):
     """
     Pré-processa a imagem convertendo-a para escala de cinza e binarizando.
     :param image_path: Caminho para a imagem de entrada.
-    :param output_path: Caminho para salvar a imagem processada (opcional).
-    :return: Caminho para a imagem processada ou a imagem processada.
+    :return: Imagem processada.
     """
     image = cv2.imread(image_path)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, binary_image = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY)
-    
-    if output_path:
-        cv2.imwrite(output_path, binary_image)
-        return output_path
-    else:
-        return binary_image
+    return binary_image
 
 def extract_text(image_path, lang='eng', preprocess=False):
     """
@@ -28,16 +22,11 @@ def extract_text(image_path, lang='eng', preprocess=False):
     :return: Texto extraído.
     """
     if preprocess:
-        # Pré-processar a imagem e salvar como uma imagem temporária
         processed_image = preprocess_image(image_path)
-        # Converter a imagem processada para um formato PIL para o pytesseract
         image = Image.fromarray(processed_image)
     else:
-        # Carregar a imagem diretamente
         image = Image.open(image_path)
     
-    # Extrair texto usando pytesseract
     custom_config = f'--oem 3 --psm 6 -l {lang}'
     text = pytesseract.image_to_string(image, config=custom_config)
-    
     return text
